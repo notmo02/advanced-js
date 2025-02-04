0b5e87b483b76dbf7032b642ff20485032d540ac
notmo02 <mohammeda7605@nycstudents.net>
2025-02-04 14:53:33 +0000

    changes pt.2

--- LAB1/practice/script.js
+++ LAB1/practice/script.js
@@ -24,18 +24,74 @@ age = 30;
 age = 40;
 age = 50;
 
-alert(age);
+// alert(age);
 
 // Essential Operations
 
+let sum = age + 2; ///42
+console.log(sum);
+
+// sum = sum + 10;
+// sum += 10;
+// console.log(sum);
+// sum = sum - 4
+// sum -= 4;
+// console.log(sum);
+// sum = sum / 2
+// sum /= 2;
+// console.log(sum);
+// sum = sum * 5
+// sum *= 5;
+// console.log(sum);
+
+
 // Additional operations
 
-// User Input
+// equal to
+console.log(34 == 72);
+// less than
+console.log(34 < 72);
+// greater than
+console.log(34 > 72);
+// less than or equal to
+console.log(34 <= 72);
+// greater than or equal to
+console.log(34 >= 72);
+// not equal to
+console.log(34 != 72);
 
+console.log("The answer is:", "10" == 10);
+
+// User Input
+// let friend = prompt("who is your best friend?")
+
+// Method 1
+// console.log("Your best friend is" + friend);
+
+// // Method 2
+// console.log("Your best friend is", friend);
+
+// // Method 3
+// console.log(`Your best friend is ${friend}`);
 
 // Concatenation v. templates
 
-// String methods
+// String methods (uppercase, lowercase, includes)
+const name1 = "daniel";
+console.log(name1.toUpperCase());
+// toLowerCase and other string methods
+const name2 = "MOHSIR"
+console.log(name2.toLowerCase());
+
+const tax = "fanum tax";
+console.log(tax.includes("fan"));
+
+// Math methods
+console.log(Math.random() * 100);
+
+console.log(Math.floor(10.6));
+
+// Math methods - https://www.w3schools.com/js/js_math.asp
 
 
 // Comparison operators - true or false?
